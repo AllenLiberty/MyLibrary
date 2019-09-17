d3547c0d86ef4650f8991cc66465ea7a8403c0e2
allen <wuning1990@126.com>
2019-09-17 17:19:38 +0800

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+struct MyLibrary {
+    var text = "Hello, World!"
+}
+
+class ConfigWifiVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
